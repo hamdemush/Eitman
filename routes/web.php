9837aefed653a9ba03ec564f26547c8bab538e25
hamdemush <hmushtaha6@smail.ucas.edu.ps>
2026-08-22 15:28:07 +0300

    fix logo and navbar links

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
-Route::get('/{any}', function () {
-    return view('index');
-})->where('any', '^(?!api).*$');
