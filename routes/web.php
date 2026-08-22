28ca9f5a4c93eb3dd4b24549b8b2bc2510a7d9b4
hamdemush <hmushtaha6@smail.ucas.edu.ps>
2026-08-22 16:50:13 +0300

    Add web routes and update blade templates

--- routes/web.php
+++ routes/web.php
@@ -13,46 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/specialists', function () {
-    return view('pages.specialists');
-});
-
-Route::get('/specialist-profile', function () {
-    return view('pages.specialist-profile');
-});
-
-Route::get('/articles', function () {
-    return view('pages.articles');
-});
-
-Route::get('/about', function () {
-    return view('pages.about');
-});
-
-Route::get('/emergency', function () {
-    return view('pages.emergency');
-});
-
-Route::get('/login', function () {
-    return view('pages.login');
-});
-
-Route::get('/register', function () {
-    return view('pages.register');
-});
-
-Route::get('/assessment', function () {
-    return view('pages.assessment');
-});
-
-Route::get('/privacy', function () {
-    return view('pages.privacy');
-});
-
-Route::get('/terms', function () {
-    return view('pages.terms');
-});
-
-Route::get('/admin/dashboard', function () {
-    return view('pages.admin.dashboard');
+Route::get('/', function () {
+    return view('index');
 });
